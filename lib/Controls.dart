8b009190ce3aba9f10a49b3744a40fc9caed94e8
ggarushi <garushiarora1999@gmail.com>
2021-03-08 00:49:55 +0530

    Controls added

--- lib/Controls.dart
+++ lib/Controls.dart
@@ -1,1 +1,53 @@
 import 'package:flutter/material.dart';
+class PlayerControls extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Control(
+            icon: Icons.repeat,
+          ),
+          Control(
+            icon: Icons.skip_previous,
+          ),
+//          PlayControl(),
+          Control(
+            icon: Icons.skip_next,
+          ),
+          Control(
+            icon: Icons.shuffle,
+          ),
+        ],
+      ),
+    );
+  }
+}
+class Control extends StatelessWidget {
+  final IconData icon;
+  const Control({this.icon});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: 60,
+      decoration: BoxDecoration(
+        color: Theme.of(context).primaryColor,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+              color: Theme.of(context).primaryColorDark.withOpacity(0.5),
+              offset: Offset(5, 10),
+              spreadRadius: 3,
+              blurRadius: 10),
+          BoxShadow(
+              color: Colors.white,
+              offset: Offset(-3, -4),
+              spreadRadius: -2,
+              blurRadius: 20)
+        ],
+      ),
+    );
+  }
+}
